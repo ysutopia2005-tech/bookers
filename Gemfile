891436a46beee85e923d165fc801707b5eada0ce
Ubuntu <ubuntu@ip-172-31-45-71.ap-northeast-1.compute.internal>
2025-09-22 02:26:44 +0000

    [finish]Bookersを作成しよう

--- Gemfile
+++ Gemfile
@@ -41,16 +41,16 @@ group :development do
   gem 'rack-mini-profiler', '~> 2.0'
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
 end
 
 group :test do
-  # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 3.26'
-  gem 'selenium-webdriver', '>= 4.0.0.rc1'
-  # Easy installation and use of web drivers to run system tests with browsers
-  gem 'webdrivers'
+  gem 'capybara', '>= 2.15'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'net-smtp'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "Book was not successfully"
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -23,9 +29,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "Book was not successfully updated"
+      render :edit
+    end
   end
 
   def destroy
